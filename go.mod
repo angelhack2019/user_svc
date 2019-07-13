fd3403b2e037992bc0854bea045776fa9ee150c8
Jacob Landowski <j.landowski@f5.com>
2019-07-13 15:03:27 -0700

    Start boilerplate for login routing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/angelhack2019/user_svc
+
+go 1.12
+
+require (
+	github.com/angelhack2019/lib v0.0.0-20190713215402-f41175a9f525
+	github.com/gorilla/mux v1.7.3
+)
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/angelhack2019/user_svc/controllers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	initRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initRoutes() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.Host("localhost").Schemes("http")
+
+	router.HandleFunc("/", controllers.HandleHome).Methods("GET")
+	router.HandleFunc("/login", controllers.HandleLogin).Methods("GET")
+	router.HandleFunc("/login/auth", controllers.HandleAuth).Methods("POST")
+
+	http.Handle("/", router)
+}
